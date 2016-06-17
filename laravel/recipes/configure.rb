d92737e282b8b3a74c8ae666efd37f45c78c03a2
Raffaele <izziaraffaele@gmail.com>
2016-06-17 13:42:40 +0200

    Move the supervisor config file

--- laravel/recipes/configure.rb
+++ laravel/recipes/configure.rb
@@ -27,4 +27,15 @@ node[:deploy].each do |application, deploy|
       command "mv .env.production .env"
   end
 
+  # move supervisord configurations
+  execute "mv supervisord.conf /etc/supervisor/conf.d/" do
+      cwd "#{deploy[:deploy_to]}/current"
+      user "root"
+      command <<-EOH
+    rm /etc/supervisor/conf.d/supervisord.laravel.conf
+    cp supervisord.conf /etc/supervisor/conf.d/supervisord.laravel.conf
+    rm supervisord.conf
+    EOH
+      only_if { ::File.exist? "#{deploy[:deploy_to]}/current/supervisord.conf" }
+  end
 end
